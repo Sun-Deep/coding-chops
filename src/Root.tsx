1f0891326a5c7fa24fd79702712912e8522853d7
Sun-Deep <sundeepp29@gmail.com>
2026-09-06 18:35:21 +0545

    chore: register new Remotion compositions and renders

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,11 +1,25 @@
 import "./index.css";
 import { Folder } from "remotion";
+import { BrandCompositions } from "./shared/brand/social/compositions";
+import { ProblemSolvingCompositions } from "./tracks/problem-solving/compositions";
 import { SystemDesignCompositions } from "./tracks/system-design/compositions";
+import { VerticalCompositions } from "./vertical/compositions";
 
 export const RemotionRoot: React.FC = () => {
   return (
-    <Folder name="System-Design">
-      <SystemDesignCompositions />
-    </Folder>
+    <>
+      <Folder name="System-Design">
+        <SystemDesignCompositions />
+      </Folder>
+      <Folder name="Problem-Solving">
+        <ProblemSolvingCompositions />
+      </Folder>
+      <Folder name="Vertical">
+        <VerticalCompositions />
+      </Folder>
+      <Folder name="Brand">
+        <BrandCompositions />
+      </Folder>
+    </>
   );
 };
